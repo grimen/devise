3725283927b44760e6b54503de690f4be5fbcc9f
Jonas Grimfelt <grimen@gmail.com>
2010-01-25 10:53:44 +0100

    Making MongoMapper tests only run if MongoMapper is available - and if MongoDB server is running. If not; print out to console an alert on how to make these tests to run. A bit leaner than throwing an unhandled exception, as MongoMapper is optional dependency.

--- test/orm/mongo_mapper.rb
+++ test/orm/mongo_mapper.rb
@@ -1,20 +1,37 @@
-require 'mongo_mapper'
-MongoMapper.database = "devise-test-suite"
-MongoMapper.connection = Mongo::Connection.new('127.0.0.1', 27017)
-
-require File.join(File.dirname(__FILE__), '..', 'rails_app', 'config', 'environment')
-require 'test_help'
-
-module MongoMapper::Document
-  # TODO This should not be required
-  def invalid?
-    !valid?
+begin
+  require 'mongo_mapper'
+rescue LoadError
+  begin
+    gem 'mongo_mapper'
+    require 'mongo_mapper'
+  rescue LoadError
   end
 end
 
-class ActiveSupport::TestCase
-  setup do
-    User.delete_all
-    Admin.delete_all
+if defined?(MongoMapper)
+  begin
+    MongoMapper.database = "devise-test-suite"
+    MongoMapper.connection = Mongo::Connection.new('127.0.0.1', 27017) rescue raise
+
+    require File.join(File.dirname(__FILE__), '..', 'rails_app', 'config', 'environment')
+    require 'test_help'
+
+    module MongoMapper::Document
+      # TODO This should not be required
+      def invalid?
+        !valid?
+      end
+    end
+
+    class ActiveSupport::TestCase
+      setup do
+        User.delete_all
+        Admin.delete_all
+      end
+    end
+  rescue
+    puts "** ALERT: MongoDB could not be established to given host:port (127.0.0.1:27017). To run MongoMapper-tests for Devise, please ensure the MongoDB server is running."
   end
+else
+  puts "** ALERT: MongoMapper could not be loaded. To run MongoMapper-tests for Devise, please install MongoMapper gem using: sudo gem install mongo_mapper."
 end
