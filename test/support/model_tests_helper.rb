0d53e3ad785204ae06f9e843ef41083877863218
Jonas Grimfelt <grimen@gmail.com>
2010-01-25 11:41:56 +0100

    Make test/support/* less error prone to: "superclass mismatch for class TestCase (TypeError)"

--- test/support/model_tests_helper.rb
+++ test/support/model_tests_helper.rb
@@ -1,36 +1,40 @@
-class ActiveSupport::TestCase
-  def setup_mailer
-    ActionMailer::Base.deliveries = []
-  end
+require 'active_support/test_case'
 
-  def store_translations(locale, translations, &block)
-    begin
-      I18n.backend.store_translations locale, translations
-      yield
-    ensure
-      I18n.reload!
+module Devise
+  module ModelTestsHelper
+    def setup_mailer
+      ActionMailer::Base.deliveries = []
     end
-  end
 
-  # Helpers for creating new users
-  #
-  def generate_unique_email
-    @@email_count ||= 0
-    @@email_count += 1
-    "test#{@@email_count}@email.com"
-  end
+    def store_translations(locale, translations, &block)
+      begin
+        I18n.backend.store_translations locale, translations
+        yield
+      ensure
+        I18n.reload!
+      end
+    end
 
-  def valid_attributes(attributes={})
-    { :email => generate_unique_email,
-      :password => '123456',
-      :password_confirmation => '123456' }.update(attributes)
-  end
+    # Helpers for creating new users
+    #
+    def generate_unique_email
+      @@email_count ||= 0
+      @@email_count += 1
+      "test#{@@email_count}@email.com"
+    end
 
-  def new_user(attributes={})
-    User.new(valid_attributes(attributes))
-  end
+    def valid_attributes(attributes={})
+      { :email => generate_unique_email,
+        :password => '123456',
+        :password_confirmation => '123456' }.update(attributes)
+    end
 
-  def create_user(attributes={})
-    User.create!(valid_attributes(attributes))
+    def new_user(attributes={})
+      User.new(valid_attributes(attributes))
+    end
+
+    def create_user(attributes={})
+      User.create!(valid_attributes(attributes))
+    end
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,10 +6,24 @@ DEVISE_ORM = (ENV["DEVISE_ORM"] || :active_record).to_sym
 puts "\n==> Devise.orm = #{DEVISE_ORM.inspect}"
 require File.join(File.dirname(__FILE__), 'orm', DEVISE_ORM.to_s)
 
+require 'test/unit'
 require 'webrat'
 require 'mocha'
+require 'active_support'
+require 'action_controller'
+require 'action_view'
+require 'action_mailer'
 
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
+
+ActiveSupport::TestCase.class_eval do
+  include Devise::AssertionsHelper
+  include Devise::ModelTestsHelper
+end
+
+ActionController::IntegrationTest.class_eval do
+  include ::Devise::IntegrationTestsHelper
+end
 
 ActionMailer::Base.delivery_method = :test
 ActionMailer::Base.perform_deliveries = true
